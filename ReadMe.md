dc71c52312d8ec15277e5de938b03b1ffffa88a9
William-Weng <linuxice0609@gmail.com>
2018-02-25 21:18:51 +0800

    SafeArea_iOS11 - 利用Autolayout處理SafeArea

--- ReadMe.md
+++ ReadMe.md
@@ -2,3 +2,6 @@
 
 ## LayerAnimation - 利用遮罩做動畫
 ![LayerAnimation - 利用遮罩做動畫](./LayerAnimation.gif)
+
+## SafeArea_iOS11 - 利用Autolayout處理SafeArea
+![SafeArea_iOS11 - 利用Autolayout處理SafeArea](./SafeArea_iOS11.gif)
---
+++ SafeArea_iOS11/SafeArea_iOS11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafeArea_iOS11
+//
+//  Created by William-Weng on 2018/2/25.
+//  Copyright © 2018年 William-Weng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
